24a4c43b2fafab9fc545d87d754d318551ba332a
Cecep Abdul Rahman <cecep.rahman@stickearn.com>
2023-04-20 04:38:18 +0700

    Update footer

--- src/components/GuestBook.vue
+++ src/components/GuestBook.vue
@@ -21,20 +21,19 @@ input, textarea, select, option {
 <template>
   <section class="w-full bg-slate-100 pt-5">
     <section class="container-section bg-slate-100">
-      <HeaderSection title="Buku Tamu" subtitle="Demi kelancaran acara dimohon untuk para tamu undangan untuk memastikan kehadirannya pada acara kami" />
-      <!-- Form -->
+      <!-- Form 
       <form 
         ref="form"
         @submit="sendMessage"
         class="w-10/12 mx-auto mt-6">
-        <!-- Alert -->
+       
         <Alert :statusResponse="statusResponse" :showAlert="showAlert" v-on:close="showAlert = false" />
-        <!-- Guest Name -->
+        
         <div class="input-wrapper" data-aos="zoom-in">
           <label for="GuestName">Nama</label>
           <input v-model="GuestName" placeholder="Nama lengkap anda" name="GuestName" id="GuestName" type="text" required>
         </div>
-        <!-- Guest Status -->
+       
         <div class="input-wrapper" data-aos="zoom-in">
           <label for="GuestStatus">Kehadiran</label>
           <select v-model="GuestStatus" name="GuestStatus" id="GuestStatus" required>
@@ -42,13 +41,13 @@ input, textarea, select, option {
             <option value="Tidak Hadir">Tidak Hadir</option>
           </select>
         </div>
-        <!-- Guest Message -->
+       
         <div class="input-wrapper" data-aos="zoom-in">
           <label for="GuestMessage">Pesan</label>
           <textarea placeholder="Tuliskan pesan anda disini" v-model="GuestMessage" name="GuestMessage" id="GuestMessage" cols="30" rows="5" required></textarea>
         </div>
         {{ query }}
-        <!-- Submit -->
+       
         <button 
           data-aos="zoom-in"
           class="w-full bg-gray-800 text-gray-100 mt-6 rounded-lg py-2 font-medium pointer active:scale-90 hover:border border-gray-500 hover:bg-gray-100 hover:text-green-500 duration-300" type="submit">
@@ -56,13 +55,14 @@ input, textarea, select, option {
           Kirim pesan
         </button>
       </form>
+      -->
       <!-- Gift Section -->
       <Gift></Gift>
       <!-- Message Box -->
       <!--MessagesBox :messages="messages" -->
       <!-- Frames -->
       <div class="w-full text-center pb-12 mt-12">
-        <p class="text-sm text-amber-600 font-medium">Turut Mengundang &copy; 2023</p>
+        <p class="text-sm text-amber-600 font-medium">Wedding Invitation by <a href="https://wa.link/6f93hg"><strong>Turut Mengundang</strong></a> &copy; 2023</p>
       </div>
     </section>
   </section>
