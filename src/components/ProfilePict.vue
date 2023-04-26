552946eb5ce517fc36e14cc103f9cb676ec6d3b6
Cecep Abdul Rahman <cecep.rahman@stickearn.com>
2023-04-26 09:25:42 +0700

    Update styling frame

--- src/components/ProfilePict.vue
+++ src/components/ProfilePict.vue
@@ -1,7 +1,7 @@
 <template>
   <section 
     class="w-full mb-6">
-    <img data-aos="zoom-in" class="w-9/12 shadow-lg border border-gray-200 mx-auto rounded-tr-3xl rounded-bl-3xl" :src="source.thumb" :alt="source.thumb">
+    <img data-aos="zoom-in" class="w-9/12 shadow-lg border border-gray-200 mx-auto rounded-3xl" :src="source.thumb" :alt="source.thumb">
     <div data-aos="slide-up" class="mt-10 w-full text-center text-slate-800 text-sm">
       <p class="text-gray-50 text-[2rem] font-semibold tangerine-font">{{ source.name }}</p>
       <p class="text-gray-100 mb-3 mt-1 font-semibold">Anak dari pasangan Bapak {{ source.parents[0] }} dengan Ibu {{ source.parents[1] }}</p>
